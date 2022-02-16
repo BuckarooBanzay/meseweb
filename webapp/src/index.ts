6df7cd41d820fb95cb655b1cca102c5385b49320
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-02-16 21:06:45 +0100

    ws relay

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -2,3 +2,5 @@
 import * as srp from "secure-remote-password/client";
 
 console.log("ok", srp);
+
+//ws = new WebSocket("ws://127.0.0.1:8080/api/ws?host=minetest&port=30000")
--- ws.go
+++ ws.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,34 +19,75 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+func SendError(w http.ResponseWriter, code int, message string) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write([]byte(message))
+}
+
 func (t *WS) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Printf("Websocket request from %s\n", req.RemoteAddr)
+	target_host := req.URL.Query().Get("host")
+	target_port_str := req.URL.Query().Get("port")
+	fmt.Printf("Websocket request from %s to %s:%s\n", req.RemoteAddr, target_host, target_port_str)
 
-	conn, err := upgrader.Upgrade(resp, req, nil)
-
+	target_port, err := strconv.ParseInt(target_port_str, 10, 64)
 	if err != nil {
-		fmt.Printf("Websocket error: %s\n", err.Error())
+		SendError(resp, 500, fmt.Sprintf("Invalid port: %s", target_port_str))
 		return
 	}
 
+	conn, err := upgrader.Upgrade(resp, req, nil)
+	if err != nil {
+		SendError(resp, 500, fmt.Sprintf("Websocket error: %s\n", err.Error()))
+		return
+	}
+
+	udp_conn, err := net.Dial("udp", fmt.Sprintf("%s:%d", target_host, target_port))
+	if err != nil {
+		SendError(resp, 500, fmt.Sprintf("udp socket error: %s\n", err.Error()))
+		return
+	}
+
+	err_chan := make(chan error, 10)
+
+	// udp -> ws
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			len, err := udp_conn.Read(buf)
+			if err != nil {
+				err_chan <- err
+				return
+			}
+
+			fmt.Printf("Relaying %d bytes to the websocket\n", len)
+			err = conn.WriteMessage(0, buf[:len])
+			if err != nil {
+				err_chan <- err
+				return
+			}
+		}
+	}()
+
+	// ws -> udp
 	go func() {
 		for {
 			_, data, err := conn.ReadMessage()
 			if err != nil {
-				fmt.Printf("Websocket rx-error: %s\n", err.Error())
+				err_chan <- err
 				return
 			}
-			fmt.Printf("Rx: %s\n", string(data))
+
+			fmt.Printf("Relaying %d bytes to the udp socket\n", len(data))
+			_, err = udp_conn.Write(data)
+			if err != nil {
+				err_chan <- err
+				return
+			}
 		}
 	}()
 
-	ch := make(chan []byte)
-	for data := range ch {
-		err := conn.WriteMessage(websocket.TextMessage, data)
-		if err != nil {
-			break
-		}
-	}
-
-	close(ch)
+	err = <-err_chan
+	fmt.Printf("ws-error: %s\n", err.Error())
+	conn.Close()
 }
