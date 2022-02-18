e63da656261d14a0538948109d33dfc31a564dd2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-02-18 08:21:59 +0100

    srp stuff

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -15,19 +15,18 @@ const ws = new WebSocket("ws://127.0.0.1:8080/api/ws?host=minetest&port=30000")
 ws.onerror = console.log.bind(console)
 ws.onclose = console.log.bind(console)
 
+const username = "test2"
+const password = "enter"
+
 const client = new Client(ws)
 client.addReadyListener(function(c){
     setTimeout(function(){
         console.log("Sending INIT")
-        c.sendCommand(new ClientInit("test"))
+        c.sendCommand(new ClientInit(username))
     }, 1000)
 })
 
 const eph = srp.generateEphemeral()
-const username = "test"
-const password = "enter"
-const salt = srp.generateSalt()
-const privateKey = srp.derivePrivateKey(salt, username, password)
 
 client.addCommandListener(function(cmd){
     console.log("Received command: " + JSON.stringify(cmd))
@@ -36,23 +35,35 @@ client.addCommandListener(function(cmd){
 
         if (cmd.authMechanismFirstSrp) {
             // works
+            const salt = srp.generateSalt()
+            const privateKey = srp.derivePrivateKey(salt, username, password)
             const verifier = srp.deriveVerifier(privateKey)
-            const firstSrp = new ClientFirstSRP(hexToArray(salt), hexToArray(verifier))
-            client.sendCommand(firstSrp)
+            client.sendCommand(new ClientFirstSRP(hexToArray(salt), hexToArray(verifier)))
 
         } else if (cmd.authMechanismSrp) {
             // Server: User test at 192.168.32.3 supplied wrong password (auth mechanism: SRP).
-            const bytesA = new ClientSRPBytesA(hexToArray(eph.public))
-            client.sendCommand(bytesA)
+            const a = hexToArray(eph.public)
+            if (a.length != 256){
+                throw new Error("invalid bytes-a length")
+            }
+            client.sendCommand(new ClientSRPBytesA(a))
 
         }
     }
 
     if (cmd instanceof ServerSRPBytesSB) {
-        console.log("Got server bytes S+B")
+        console.log("Got server bytes S+B") // salt + server-public
 
-        const session = srp.deriveSession(eph.secret, arrayToHex(cmd.bytesB), salt, username, privateKey)
-        const proof = hexToArray(session.proof)
+        const serverSalt = arrayToHex(cmd.bytesS)
+        const serverPublic = arrayToHex(cmd.bytesB)
+
+        const privateKey = srp.derivePrivateKey(serverSalt, username, password)
+        const clientSession = srp.deriveSession(eph.secret, serverPublic, serverSalt, username, privateKey)
+
+        const proof = hexToArray(clientSession.proof)
+        if (proof.length != 32) {
+            throw new Error("invalid proof length")
+        }
         client.sendCommand(new ClientSRPBytesM(proof))
     }
 })
