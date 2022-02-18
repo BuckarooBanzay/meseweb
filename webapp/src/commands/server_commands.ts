86c80b74da3dba28a8b7903c75a282c5aaef199e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-02-18 15:21:29 +0100

    partial chat message support

--- webapp/src/commands/server_commands.ts
+++ webapp/src/commands/server_commands.ts
@@ -1,6 +1,7 @@
 import { ServerCommand } from "./command";
 import { ServerAccessDenied } from "./server_access_denied";
 import { ServerAuthAccept } from "./server_auth_accept";
+import { ServerChatMessage } from "./server_chat_message";
 import { ServerHello } from "./server_hello";
 import { ServerSRPBytesSB } from "./server_srp_bytes_s_b";
 import { ServerTimeOfDay } from "./server_time_of_day";
@@ -11,6 +12,7 @@ export function getServerCommand(commandId: number): ServerCommand|null {
         case 0x03: return new ServerAuthAccept()
         case 0x0A: return new ServerAccessDenied()
         case 0x29: return new ServerTimeOfDay()
+        case 0x2F: return new ServerChatMessage()
         case 0x60: return new ServerSRPBytesSB()
     }
 
