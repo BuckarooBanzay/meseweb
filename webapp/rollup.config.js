87022846f4504173f49cc156fd443226cfadb1d0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-02-24 12:57:13 +0100

    Revert "use esbuild"
    This reverts commit a6952cb289c3ab44eb8561541e26b9b387dd5467.

--- webapp/rollup.config.js
+++ webapp/rollup.config.js
@@ -1,7 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from "rollup-plugin-terser";
 import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild';
-
 
 export default {
   input: 'src/index.ts',
@@ -15,8 +15,11 @@ export default {
       browser: true
     }),
     commonjs(),
-    esbuild({
-      minify: true
+    typescript(),
+    terser({
+      output: {
+        comments: false
+      }
     })
   ],
   onwarn: function(warning) {
