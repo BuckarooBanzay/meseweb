63da1ea0888e987fd52a20bcf957a5516b2706f7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-01 20:33:55 +0100

    packet stuff

--- webapp/src/command/CommandClient.ts
+++ webapp/src/command/CommandClient.ts
@@ -1,7 +1,18 @@
-import { unmarshal } from "../packet/marshal";
+import { marshal, unmarshal } from "../packet/marshal";
+import { Packet } from "../packet/Packet";
+import { createCommandPacket, createDisconnect } from "../packet/packetfactory";
+import { setSeqNr } from "../packet/sequence";
+import { SplitPacketHandler } from "../packet/splitpackethandler";
+import { ControlType, PacketType } from "../packet/types";
+import { ClientInit } from "./client/ClientInit";
 import { ClientCommand } from "./ClientCommand";
+import { getServerCommand } from "./ServerCommand";
 
 export class CommandClient {
+
+    peerId = 0
+    splitHandler = new SplitPacketHandler()
+
     constructor(public ws: WebSocket) {
         ws.addEventListener("open", () => this.onOpen())
         ws.addEventListener("message", ev => this.onMessage(ev))
@@ -12,15 +23,71 @@ export class CommandClient {
     }
 
     private onMessage(ev: MessageEvent<Blob>) {
-        ev.data.arrayBuffer().then(function(ab){
+        ev.data.arrayBuffer().then(ab => {
             const buf = new Uint8Array(ab);
             const p = unmarshal(buf);
             console.log(p)
-            //TODO
+            if (p.packetType == PacketType.Reliable){
+                if (p.controlType == ControlType.SetPeerID){
+                    // set peer id
+                    this.peerId = p.peerId;
+                    console.log("Set peerId to " + this.peerId);
+                    return;
+                }
+    
+                if (p.subType == PacketType.Original){
+                    this.parseCommandPayload(p.payloadView);
+                }
+    
+                if (p.subType == PacketType.Split) {
+                    const payload = this.splitHandler.AddSplitPacket(p);
+                    if (payload != null) {
+                        // all split parts arrived
+                        this.parseCommandPayload(new DataView(payload.buffer));
+                    }
+                }
+            }
         })
     }
 
-    SendCommand(cmd: ClientCommand) {
-        //TODO
+    private parseCommandPayload(dv: DataView) {
+        if (this.ws.readyState != WebSocket.OPEN){
+            return;
+        }
+
+        const cmdId = dv.getUint16(0);
+        try {
+            const cmd = getServerCommand(cmdId);
+            if (cmd != null){
+                cmd.UnmarshalPacket(new DataView(dv.buffer, dv.byteOffset + 2));
+                //TODO: propagate
+            } else {
+                console.log("Unknown command received: " + cmdId);
+            }
+
+        } catch (e) {
+            console.error(e);
+            console.log("Caught error, aborting");
+            this.close();
+        }
+    }
+
+    close() {
+        this.SendPacket(createDisconnect())
+        this.ws.close()
+    }
+
+    SendPacket(p: Packet) {
+        p.peerId = this.peerId
+        this.ws.send(marshal(p))
+    }
+
+    SendCommand(cmd: ClientCommand, type: PacketType) {
+        const packets = createCommandPacket(cmd, this.peerId, type || PacketType.Reliable);
+        packets.forEach(p => this.SendPacket(p));
+
+        if (cmd instanceof ClientInit){
+            setSeqNr(65500-1);
+        }
     }
 }
--- webapp/src/command/ServerCommand.ts
+++ webapp/src/command/ServerCommand.ts
@@ -1,3 +1,12 @@
+import { ServerHello } from "./server/ServerHello"
+
 export interface ServerCommand {
     UnmarshalPacket(dv: DataView): void
+}
+
+export function getServerCommand(commandId: number): ServerCommand | null {
+    switch (commandId) {
+        case 0x02: return new ServerHello
+    }
+    return null
 }
