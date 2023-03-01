3cfbce967ce4d960e638053bf2587e05ce60658a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-01 19:49:44 +0100

    cmd client wip

--- webapp/src/command/CommandClient.ts
+++ webapp/src/command/CommandClient.ts
@@ -1,7 +1,24 @@
+import { unmarshal } from "../packet/marshal";
 import { ClientCommand } from "./ClientCommand";
 
 export class CommandClient {
-    constructor(public ws: WebSocket) {}
+    constructor(public ws: WebSocket) {
+        ws.addEventListener("open", () => this.onOpen())
+        ws.addEventListener("message", ev => this.onMessage(ev))
+    }
+
+    private onOpen() {
+        console.log("websocket opened")
+    }
+
+    private onMessage(ev: MessageEvent<Blob>) {
+        ev.data.arrayBuffer().then(function(ab){
+            const buf = new Uint8Array(ab);
+            const p = unmarshal(buf);
+            console.log(p)
+            //TODO
+        })
+    }
 
     SendCommand(cmd: ClientCommand) {
         //TODO
