037b5c85c760afec8191433107d70b95bfc191e5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-01 19:21:56 +0100

    packet classes

---
+++ webapp/src/packet/Packet.ts
@@ -0,0 +1,32 @@
+import { ControlType, PacketType } from "./types";
+
+export class Packet {
+
+    packetType = PacketType.Control
+    controlType = ControlType.Ack
+    subType = PacketType.Control
+    peerId!: number
+    seqNr = -1
+    channel!: number
+    payload = new Uint8Array()
+    payloadView = new DataView(this.payload.buffer)
+
+    dumpPayload() {
+        const a = new Array(this.payloadView.byteLength);
+        for (let i=0; i<this.payloadView.byteLength; i++){
+            a[i] = this.payloadView.getUint8(i);
+        }
+        return a;
+    }
+
+    toString() {
+        return `Packet{`+
+            `peerId=${this.peerId},`+
+            `seqNr=${this.seqNr},`+
+            `channel=${this.channel},`+
+            `type=${PacketType[this.packetType]},`+
+            `control=${ControlType[this.controlType]},`+
+            `subType=${PacketType[this.subType]}`+
+        `}`;
+    }
+}
