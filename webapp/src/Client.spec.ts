2ac2a7d0d2264ba02e9607f802d60b4606822a95
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-06 13:34:52 +0100

    client test

---
+++ webapp/src/Client.spec.ts
@@ -0,0 +1,39 @@
+import NodeWebSocket from "ws"
+import { env } from "process"
+import Logger from "js-logger"
+import { CommandClient } from "./command/CommandClient"
+import { Client } from "./Client"
+
+describe("Client", function() {
+    if (env.INTEGRATION_TEST != "true") {
+        test.only("skip", function() {})
+    }
+
+    //Logger.useDefaults({ defaultLevel: Logger.DEBUG })
+
+    test("Login", function(done){
+        const host = "minetest"
+        const port = 30000
+        const url = `ws://server:8080/api/ws?host=${host}&port=${port}`
+
+        const username = "test"
+        const password = "enter"
+
+        Logger.debug("connecting to: ", url)
+        const ws = new NodeWebSocket(url);
+        const cc = new CommandClient(ws as unknown as WebSocket)
+        const client = new Client(cc)
+
+        client.Login(username, password)
+        .then(() => {
+            done()
+        })
+        .catch(e => {
+            Logger.error(e)
+            done(e)
+        })
+        .finally(() => {
+            cc.close()
+        })
+    })
+})
---
+++ webapp/src/Client.ts
@@ -0,0 +1,63 @@
+import srp from "secure-remote-password/client"
+import { arrayToHex, hexToArray } from "./util/hex";
+import { ClientFirstSRP } from "./command/client/ClientFirstSRP";
+import { ClientInit } from "./command/client/ClientInit";
+import { ClientSRPBytesA } from "./command/client/ClientSRPBytesA";
+import { ClientSRPBytesM } from "./command/client/ClientSRPBytesM";
+import { CommandClient } from "./command/CommandClient";
+import { PacketType } from "./command/packet/types";
+import { ServerAccessDenied } from "./command/server/ServerAccessDenied";
+import { ServerAuthAccept } from "./command/server/ServerAuthAccept";
+import { ServerHello } from "./command/server/ServerHello";
+import { ServerSRPBytesSB } from "./command/server/ServerSRPBytesSB";
+
+export class Client {
+
+    eph = srp.generateEphemeral()
+
+    constructor(public cc: CommandClient) {}
+
+    Login(username: string, password: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.cc.events.once("ServerCommand", cmd => {
+                if (cmd instanceof ServerAccessDenied) {
+                    reject()
+                }
+            })
+
+            this.cc.OnReady()
+            .then(() => this.cc.PeerInit())
+            .then(() => new Promise(resolve => setTimeout(resolve, 1000)))
+            .then(() => this.cc.ExchangeCommand(new ClientInit(username), PacketType.Original, ServerHello))
+            .then(sh => {
+                if (sh.authMechanismFirstSrp) {
+                    const salt = srp.generateSalt()
+                    const private_key = srp.derivePrivateKey(salt, username, password)
+                    const verifier = srp.deriveVerifier(private_key)
+                    const cmd = new ClientFirstSRP(hexToArray(salt), hexToArray(verifier))
+                    return this.cc.ExchangeCommand(cmd, PacketType.Reliable, ServerSRPBytesSB)
+    
+                } else {
+                    const cmd = new ClientSRPBytesA(hexToArray(this.eph.public))
+                    return this.cc.ExchangeCommand(cmd, PacketType.Reliable, ServerSRPBytesSB)
+                }
+            })
+            .then(cmd => {
+                const serverSalt = arrayToHex(cmd.bytesS);
+                const serverPublic = arrayToHex(cmd.bytesB);
+        
+                const privateKey = srp.derivePrivateKey(serverSalt, username, password);
+                const clientSession = srp.deriveSession(this.eph.secret, serverPublic, serverSalt, username, privateKey);
+        
+                const proof = hexToArray(clientSession.proof);
+                return this.cc.ExchangeCommand(new ClientSRPBytesM(proof), PacketType.Reliable, ServerAuthAccept);
+            })
+            .then(aa => {
+                resolve()
+            })
+            .catch(e => {
+                reject(e)
+            })
+        })
+    }
+}
