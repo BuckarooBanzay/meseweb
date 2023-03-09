7d39e36488307c48563a06a51f024f11a1e1f34a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-09 07:25:14 +0100

    fix block decompression and parsing

--- webapp/src/block/blockparser.ts
+++ webapp/src/block/blockparser.ts
@@ -6,50 +6,44 @@ export function getNodePos(x: number, y: number, z: number) {
     return x + (y * 16) + (z * 256);
 }
 
-export function parseBlock(buf: Uint8Array): BlockData {
+function parseV29(buf: Uint8Array): BlockData {
     const b = new BlockData()
+    const uncompressedData = decompress(buf.slice(0, buf.byteLength - 1))
+    const dv = new DataView(uncompressedData.buffer)
 
-    const uncompressedData = decompress(buf)
-    console.log(uncompressedData)
-
-    const dv = new DataView(buf.buffer)
     let offset = 0
 
     const flags = dv.getUint8(offset++)
     b.underground = (flags & 0x01) == 0x01
     b.dayNightDiff = (flags & 0x02) == 0x02
     b.generated = (flags & 0x04) == 0x04 //TODO: invert?
+
     b.lightingComplete = dv.getUint16(offset)
+
     offset+=2
     b.contentWidth = dv.getUint8(offset++)
     b.paramsWidth = dv.getUint8(offset++)
 
-    const ab = dv.buffer.slice(offset)
-    const compressedMapnodes = new Uint8Array(ab)
-
-    if (compressedMapnodes[0] != 0x78 || compressedMapnodes[1] != 0x9c) {
-        throw new Error("invalid zlib magic")
-    }
-
-    // TODO: https://github.com/nodeca/pako/blob/master/lib/zlib/inflate.js
-    const inflate = new Inflate()
-
-    for (let i=0; i<compressedMapnodes.length; i++) {
-        inflate.push(compressedMapnodes.subarray(i,i+1), constants.Z_PARTIAL_FLUSH)
-        if (inflate.err == constants.Z_OK) {
-            break
-        }
-    }
-
-    const mapNodes = inflate.result as Uint8Array
-    if (mapNodes.length != (4096*4)){
-        throw new Error("invalid decompressed size")
-    }
-
-    b.mapNodes = new DataView(mapNodes)
+    b.mapNodes = new DataView(uncompressedData.buffer, offset, offset+(4096*2))
     for (let i=0; i<4096; i++){
         b.blockMapping.set(b.mapNodes.getUint16(i*2), true)
     }
 
     return b
+}
+
+export function parseBlock(buf: Uint8Array): BlockData {
+    const b = new BlockData()
+    const dv = new DataView(buf.buffer)
+
+    if (dv.getUint8(0) == 0x28 &&
+        dv.getUint8(1) == 0xb5 &&
+        dv.getUint8(2) == 0x2f &&
+        dv.getUint8(3) == 0xfd
+        ) {
+            // zstd magic
+            return parseV29(buf)
+    } else {
+        throw new Error("map format not supported")
+    }
 }
